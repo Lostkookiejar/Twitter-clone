e49cd73ababc83f5d0f065cc262179cfef3f88f7
Chong Rui Xin <rxchong1123@gmail.com>
2026-03-30 10:47:45 +0800

    init commit - AuthPage added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="*" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
